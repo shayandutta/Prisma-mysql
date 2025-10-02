4a5c045c3b304d02f62d80d15dabd329e7f2de47
shayandutta <shayandutta23@gmail.com>
2025-10-02 12:23:19 +0530

    nested write

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { PrismaClient } from "@prisma/client";
 const app = express();
 
 const prisma = new PrismaClient({
-  //log: ["query"], //we can add configurations like this inside the prismaClient. This particular config will log the sql querry whenever a req is hit
+  log: ["query"], //we can add configurations like this inside the prismaClient. This particular config will log the sql querry whenever a req is hit
 });
 
 app.use(bodyParser.json());
@@ -14,21 +14,38 @@ app.get("/", (req, res) => {
   res.send("home route working!");
 });
 
-app.post("/articles", async (req, res) => {
-  //   await prisma.article.create({
-  //     data: req.body,
-  //   });
-  //   res.json({
-  //     success: true,
-  //   });
 
-  await prisma.article.createMany({
-    data: req.body,
-  });
-  res.json({
-    succees: true,
-  });
-});
+//nested write operations(creating user with articles)
+
+app.post("/users", async(req, res)=>{
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            articles:{
+                create: req.body.articles
+            }
+        }
+    })
+    res.json({
+        success : true
+    })
+})
+
+// app.post("/articles", async (req, res) => {
+//   //   await prisma.article.create({
+//   //     data: req.body,
+//   //   });
+//   //   res.json({
+//   //     success: true,
+//   //   });
+
+//   await prisma.article.createMany({
+//     data: req.body,
+//   });
+//   res.json({
+//     succees: true,
+//   });
+// });
 
 //1. retrieve all articles
 app.get("/articles", async (req, res) => {
@@ -36,48 +53,48 @@ app.get("/articles", async (req, res) => {
   res.json(articles);
 });
 
-//2. retrieve a particular articles
-app.get("/articles/:id", async (req, res) => {
-  const article = await prisma.article.findFirst({
-    where: {
-      id: +req.params.id, //+ -> req.params.id is a string and a '+' in front will typecast it to number
-    },
-  });
-  res.json(article);
-});
+// //2. retrieve a particular articles
+// app.get("/articles/:id", async (req, res) => {
+//   const article = await prisma.article.findFirst({
+//     where: {
+//       id: +req.params.id, //+ -> req.params.id is a string and a '+' in front will typecast it to number
+//     },
+//   });
+//   res.json(article);
+// });
 
-//3. retrieve all articles based on some condition(only DRAFTS)
-app.get("/articless", async (req, res) => {
-  const articles = await prisma.article.findMany({
-    where: {
-      state: "DRAFT ",
-    },
-  });
-  res.json(articles);
-});
+// //3. retrieve all articles based on some condition(only DRAFTS)
+// app.get("/articless", async (req, res) => {
+//   const articles = await prisma.article.findMany({
+//     where: {
+//       state: "DRAFT ",
+//     },
+//   });
+//   res.json(articles);
+// });
 
-//4. update many articles (put this first to avoid route conflicts)
-app.put("/articles/:ids", async (req, res) => {
-  const ids = req.params.ids.split(",").map((id) => +id); //splitting the ids string into an array of ids and mapping each id to a number
-  const articles = await prisma.article.updateMany({
-    where: {
-      id: { in: ids },
-    },
-    data: req.body,
-  });
-  res.json(articles);
-});
+// //4. update many articles (put this first to avoid route conflicts)
+// app.put("/articles/:ids", async (req, res) => {
+//   const ids = req.params.ids.split(",").map((id) => +id); //splitting the ids string into an array of ids and mapping each id to a number
+//   const articles = await prisma.article.updateMany({
+//     where: {
+//       id: { in: ids },
+//     },
+//     data: req.body,
+//   });
+//   res.json(articles);
+// });
 
-//5. update a single article
-app.put("/articles/:id", async (req, res) => {
-  const article = await prisma.article.update({
-    where: {
-      id: +req.params.id,
-    },
-    data: req.body,
-  });
-  res.json(article);
-});
+// //5. update a single article
+// app.put("/articles/:id", async (req, res) => {
+//   const article = await prisma.article.update({
+//     where: {
+//       id: +req.params.id,
+//     },
+//     data: req.body,
+//   });
+//   res.json(article);
+// });
 
 //delete an article
 app.delete("/articles/:id", async(req, res)=>{
